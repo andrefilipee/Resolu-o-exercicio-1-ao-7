a56459a1cf8bbf4790d6c59bd3f8f505b20a3f10
andre <andre@ROADRUNNER>
2022-10-23 13:18:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+    
+    public class areaQuadrado{
+    public static void Main(string[] args){
+
+
+          double comprimento, altura, area;
+         Console.Write("Comprimento: ");
+
+         comprimento = Convert.ToDouble(Console.ReadLine());
+
+
+
+          Console.Write("Altura: ");
+          altura = Convert.ToDouble(Console.ReadLine());
+
+        area = comprimento * altura;
+         Console.WriteLine("Área: " + area);
+
+
+
+
+
+    }   
+
+}
