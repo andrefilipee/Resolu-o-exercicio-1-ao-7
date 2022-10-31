80b168040a922d7ab13feb17266f25d0179e4e41
andrefilipe <andrefilipe.rodrigues@my.istec.pt>
2022-10-31 17:14:49 +0000

    Resolução exercicio 2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Runtime.Intrinsics.X86;
 public class areaQuadrado {
     public static void Main()
 
-    { 
+    {
         Console.WriteLine("Digite uma opção para exibir o resultado do exercício");
         Console.WriteLine(" ");
         Console.WriteLine("Para o Exercício 01 digite: 1");
@@ -34,29 +34,63 @@ public class areaQuadrado {
         //---------Exercicio 1------------------------------------------------------------------------------------------------------------------------------------------------**
 
         //decimal comprimento, altura, area;
-        // Console.Write("Comprimento: ");
+        //Console.Write("comprimento:");
 
-        // comprimento = decimal.Parse(Console.ReadLine());
-        //  Console.Write("Altura: ");
-        // altura = decimal.Parse(Console.ReadLine());
-        // area = comprimento * altura;
-        // Console.WriteLine("Área: " + area);
+        //comprimento = decimal.Parse(Console.ReadLine());
+        //Console.Write("altura: ");
+        //altura = decimal.Parse(Console.ReadLine());
+        //area = comprimento * altura;
+        //Console.WriteLine("área: " + area);
         //---------------------------------------------------------------------------------------------------------------------------------------------------------**
 
         //----Exercicio 2-----------------------------------------------------------------------------------------------------------------------------------------------------**
 
-//2 – Uma empresa pretende criar um pequeno programa que deve receber do teclado os valores
-//correspondentes aos salários brutos dos seus funcionários e apresentar os respetivo recibo de pagamento
-//(discriminação apenas do salário líquido).
+        //2 – Uma empresa pretende criar um pequeno programa que deve receber do teclado os valores
+        //correspondentes aos salários brutos dos seus funcionários e apresentar o respetivos recibos de pagamento (discriminação apenas do salário líquido).
 
-//Tenha em atenção a informação seguinte(valores fictícios):
-//• Desconto para a Segurança Social(20 % do vencimento bruto);
-//• Desconto para o IRS, aplicando as diferentes taxas:
+        //Tenha em atenção a informação seguinte(valores fictícios):
+        //• Desconto para a Segurança Social(20 % do vencimento bruto);
+        //• Desconto para o IRS, aplicando as diferentes taxas:
 
-//        -Se o vencimento bruto for inferior a 500€, há isenção;
-//        -Se o vencimento bruto for superior ou igual a 500€ e inferior a 1000€, a dedução será de 12 %;
-//        -Se o vencimento bruto for superior ou igual a 1000€ e inferior a 1500€, a dedução será 15 %;
-//        -Se o vencimento bruto for superior a 1500€, a dedução será 18 %.
+        //        -Se o vencimento bruto for inferior a 500€, há isenção;
+        //        -Se o vencimento bruto for superior ou igual a 500€ e inferior a 1000€, a dedução será de 12 %;
+        //        -Se o vencimento bruto for superior ou igual a 1000€ e inferior a 1500€, a dedução será 15 %;      200  
+        //        -Se o vencimento bruto for superior a 1500€, a dedução será 18 %.
+
+
+        double salBruto, salLiquido, segSocial, irs;
+
+        Console.WriteLine(" Exercicio 2 ");
+        Console.Write("");
+        Console.WriteLine(" Introduza o Salário Bruto: ");
+
+        salBruto = double.Parse(Console.ReadLine());
+        segSocial = salBruto * 0.20;
+
+        if (salBruto < 500)
+        {
+            irs = 500;
+        }
+        else if (salBruto >= 500 && salBruto < 1000)
+        {
+            irs = 0.12;
+        }
+        else if (salBruto >= 1000 && salBruto < 1500)
+        {
+            irs = 0.15;
+        }
+        else
+        {
+            irs = 0.18;
+        }
+
+        salLiquido = (salBruto - segSocial) - (salBruto * irs);
+
+      Console.WriteLine("O Salário Líquido é de:" + salLiquido );
+
+
+
+        
 
 
 
